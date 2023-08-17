502696fb40389627d9f47e4009b2d8ece70c1073
harshitgupta2019 <63345282+harshitgupta2019@users.noreply.github.com>
2023-08-17 17:43:16 +0530

    Update Navbar.js

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -2,24 +2,17 @@ import React, { useState } from 'react';
 import './Navbar.css'; // Import your CSS file
 
 const Navbar = () => {
-  const [isNavOpen, setIsNavOpen] = useState(false);
-
-  const toggleNav = () => {
-    setIsNavOpen(!isNavOpen);
-  };
+  
 
   return (
     <nav className="navbar">
-      <div className="navbar-container">
         <div className="navbar-logo">
-          <a href="/">Sustainable Ag Summit</a>
+          <a href="/">Agro Summit</a>
         </div>
-        <div className={`navbar-toggle ${isNavOpen ? 'active' : ''}`} onClick={toggleNav}>
-          <div className="bar"></div>
-          <div className="bar"></div>
-          <div className="bar"></div>
-        </div>
-        <ul className={`navbar-links ${isNavOpen ? 'active' : ''}`}>
+      <div className="navbar-container">
+        <input type='checkbox' class="toggle-menu"/>
+        <div className="hamburger"></div>
+        <ul className="navbar-links">
           <li><a href="/">Home</a></li>
           <li><a href="/about">About</a></li>
           <li><a href="/speakers">Speakers</a></li>

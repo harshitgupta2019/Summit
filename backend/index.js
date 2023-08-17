a520018156b9c23dfb89464214f335f74e0a22d2
harshitgupta2019 <63345282+harshitgupta2019@users.noreply.github.com>
2023-08-17 19:50:02 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,13 @@ const dotenv=require('dotenv');
 // Set up body-parser middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(cors());
+app.use(cors(
+  {
+    origin: ["https://summit-6kur.vercel.app"],
+    methods: ["POST","GET"],
+    credentials: true
+  }
+));
 dotenv.config();
 // Connect to MongoDB
 mongoose.connect(process.env.URL, {
